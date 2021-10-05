fd48667d8352d68ff10077873480dc6568b2be7a
Alessandra-Corallo <52160214+LeeWang-Dev@users.noreply.github.com>
2021-10-05 11:25:03 +0200

    install

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const PORT = 80;
 const fs = require('fs');
 const path = require('path');
+
+
+function normalizePort(val) {
+   var port = parseInt(val, 10);
+ 
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+ 
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+ 
+   return false;
+ }
+ const PORT = normalizePort(process.env.PORT || '8080');
 
 // http server
 const express = require('express');
