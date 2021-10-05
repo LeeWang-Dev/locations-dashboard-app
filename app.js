0dba5089eb7b1c8a18861f52e367a2a31f97cac8
Alessandra-Corallo <52160214+LeeWang-Dev@users.noreply.github.com>
2021-10-05 11:16:43 +0200

    install

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const PORT = 80;
 const fs = require('fs');
+const path = require('path');
 
 // http server
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('react-client/build'));
+app.use(express.static('public'));
 
 app.get('/*', (req, res) => {
    res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'));
