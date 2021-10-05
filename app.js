79ddad5ab1ec0c19bc5f688aff5615295a15677f
Alessandra-Corallo <52160214+LeeWang-Dev@users.noreply.github.com>
2021-10-05 10:15:27 +0200

    install

---
+++ app.js
@@ -0,0 +1,17 @@
+const PORT = 8080;
+const fs = require('fs');
+
+// http server
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}.`);
+});
+
