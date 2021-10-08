d57055200f54e4d698cffd366e116c756e33e48a
Alessandra-Corallo <52160214+LeeWang-Dev@users.noreply.github.com>
2021-10-08 10:06:16 +0200

    testing map

--- react-client/src/utils/constants.js
+++ react-client/src/utils/constants.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 
 //export const BASE_URL = 'http://localhost:8080';
 
 export const BASE_URL = 'https://locations-dashboard.herokuapp.com';
-=======
-//export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://locations-dashboard-herokuapp.com';
->>>>>>> 1f5e80c62db9cb558f2c6090425b25156b56fe65
 
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGVld2FuZ2RldiIsImEiOiJja2tnbDU2c2gwMHNvMndsdDF1d2pxNTQ2In0.zKeo06DtCh6fLifrbCZCFA';
